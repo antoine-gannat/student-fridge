0ccf34791cebd9dd608fe42c8d6607e5a9613e38
antoine <antoine.gannat@gmail.com>
2020-01-08 08:31:57 +0100

    remove dist file rename

--- webapp/src/components/products/Product.vue
+++ webapp/src/components/products/Product.vue
@@ -1,12 +1,19 @@
 <template>
-<!-- product => id, name et image -->
+  <!-- product => id, name et image -->
   <div>
     <div class="card">
-      <img :src="product.image" class="card-img-top" :alt="product.name">
+      <img
+        :src="product.image"
+        class="card-img-top"
+        :alt="product.name"
+      >
       <div class="card-body">
         <h5 class="card-title product-title">{{product.name}}</h5>
         <p class="card-text peremption">{{product.expiration_date}}</p>
-        <button href="#" class="btn btn-primary product-button">Je veux ce produit</button>
+        <button
+          href="#"
+          class="btn btn-primary product-button"
+        >Je veux ce produit</button>
       </div>
     </div>
   </div>
@@ -22,25 +29,22 @@ export default {
 </script>
 
 <style scoped>
-.product-title{
+.product-title {
   font-size: medium;
   font-family: Arial Black, Arial, Verdana, sans-serif;
   text-align: center;
-  color: #503D3F
+  color: #503d3f;
 }
-.peremption{
+.peremption {
   text-align: center;
 }
-.product-button{
-background: #880D1E;
-font-size: medium;
-font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
-border: #880D1E;
-margin-right: auto;
-margin-left: 10%;
-width: 80%;
-
-
+.product-button {
+  background: #880d1e;
+  font-size: medium;
+  font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+  border: #880d1e;
+  margin-right: auto;
+  margin-left: 10%;
+  width: 80%;
 }
-
 </style>
