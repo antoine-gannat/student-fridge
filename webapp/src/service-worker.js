190dfd00c1f1b30eb0449bf0711eab59f3e90f9e
Antoine GANNAT <antoine.gannat@genesys.com>
2020-02-17 16:30:09 +0100

    handle notification click

--- webapp/src/service-worker.js
+++ webapp/src/service-worker.js
@@ -5,8 +5,22 @@ self.addEventListener('push', event => {
 
     self.registration.showNotification(data.title, {
         body: data.body,
+        icon: 'img/icons/android-chrome-512x512.png',
+        vibrate: [100, 50, 100]
     });
 });
+
+self.addEventListener('notificationclick', function(e) {
+    var notification = e.notification;
+    var action = e.action;
+  
+    if (action === 'close') {
+      notification.close();
+    } else {
+      clients.openWindow('https://www.student-fridge.fr');
+      notification.close();
+    }
+  });
 
 self.addEventListener('message', (event) => {
     if (event.data && event.data.type === 'SKIP_WAITING') {
